7a81effb36e0e427c5ce199acb57da1fc953ab77
errcc <eulerrc86@gmail.com>
2022-08-26 23:17:14 -0300

    edição no pratica-1.js, com template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,9 @@
-var nome = "Sérgio"
-var email = "sergio@guru.br"
-var profissao = "professor"
-var idade = 30
-var lugar = "Irlanda"
+var nome = "Sérgio";
+var email = "sergio@guru.br";
+var profissao = "professor";
+var idade = 30;
+var lugar = "Irlanda";
+var text = `Olá, ${nome}!Somos aqui da agência de turismo Viagem dos Sonhos.Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
 
-console.log(nome)
+
+console.log(text)
